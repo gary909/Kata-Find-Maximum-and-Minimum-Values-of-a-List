ff8bce784d7fb20d50de6a325c893056647829df
gary <gary.white@lodgit.com>
2021-10-22 13:00:57 +0200

    files

---
+++ kataFindMinMax.js
@@ -0,0 +1,15 @@
+var min = function(list){
+    
+    return list[0];
+}
+
+var max = function(list){
+    
+    return list[0];
+}
+
+console.log(min([-52, 56, 30, 29, -54, 0, -110])); // Returns -110
+console.log(min([42, 54, 65, 87, 0])); // Returns 0
+console.log(min([4,6,2,1,9,63,-134,566])); // Returns 566
+console.log(min([5])); // Returns 5
+
